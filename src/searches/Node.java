186e3109aeabd2498b7ce9c793325fa769e6b336
Vidhur Vohra <dkzjunk@yahoo.com>
2012-02-07 15:37:47 -0500

    deleted median

---
+++ src/searches/Node.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package searches;
+
+/**
+ * @author 01
+ * 
+ */
+public class Node {
+	public Node left;
+	public Node right;
+	public int intData;
+	public String strData;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Node(int intData) {
+		this.intData = intData;
+	}
+	public Node(String strData){
+		this.strData = strData;
+	}
+
+}
